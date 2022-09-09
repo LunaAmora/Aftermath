e08418b979687a3a5b3781bc178cb52c132b3b9f
Pedro H Meira <pedrohmeirasilva@gmail.com>
2022-09-09 19:08:08 -0300

    basic system transition scenes

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Aftermath
+{
+    public class LevelLoader : MonoBehaviour
+    {
+        
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                LoadNextLevel();
+            }
+        }
+
+        public void LoadNextLevel()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
