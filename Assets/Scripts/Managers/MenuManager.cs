80dd585ab7f2ca879ddd86ab009a406464b6a3ea
Luna Amora <luna.mberry@gmail.com>
2022-09-10 13:35:34 -0400

    Add menu

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,21 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 namespace Aftermath
 {
     public class MenuManager : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] private Button _play_Button;
+        [SerializeField] private Button _exit_Button;
+
         void Start()
         {
-        
+            _play_Button.onClick.AddListener(OnPlay);
+            _exit_Button.onClick.AddListener(OnExit);
         }
 
-        // Update is called once per frame
-        void Update()
+        void OnPlay()
         {
-        
+            SceneManager.LoadScene("MainScene");
+        }
+
+        void OnExit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
         }
     }
 }
